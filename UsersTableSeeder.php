ff9fbd48642bf88958edb4328b90d80c000682e5
arjunmaurya04 <163818706+arjunmaurya04@users.noreply.github.com>
2024-09-15 20:06:54 +0530

    Add files via upload

---
+++ UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'invoice',
+            'email' => 'invoice@mail.com',
+            'password' => Hash::make('invoice@123'), // Use Hash::make to encrypt the password
+        ]);
+    }
+}
